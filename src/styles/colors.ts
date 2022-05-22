2dc4b46adcb62fd924afc337bdee954b2b693feb
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-21 22:42:41 -0300

    refactor: change sass per styled-components

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  black: '#232222',
+  gold: '#998C6D',
+  blue: '#D3DFF2',
+  pink: '#E0CCE2',
+  white: '#FBF9F5'
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,52 @@
+import { colors } from './colors'
+
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: ${colors.white};
+    -webkit-font-smoothing: antialiased !important;
+    text-rendering: optimizeLegibility !important;
+  }
+
+  body, input, button, textarea, select {
+    font-family: commuters-sans, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`
