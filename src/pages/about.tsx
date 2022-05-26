9105b97f16e277cf88ead377f9fb3c2d6d6cc7fe
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-26 15:40:31 -0300

    feat: add about-me section on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,9 @@
 import { Instagram } from '../components'
 
-import { Wellcome, WellcomeContent } from '../styles/pages/about'
+import { Wellcome, WellcomeContent, AboutMe, AboutMeContent } from '../styles/pages/about'
 
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function About() {
   return (
@@ -15,6 +16,26 @@ export default function About() {
           <h1>sente-se, pegue uma xícara de café e conheça mais um pouquinho sobre quem está por trás da tela</h1>
         </WellcomeContent>
       </Wellcome>
+      <AboutMe>
+        <AboutMeContent>
+          <Image src="/images/logo-symbol-black.svg" alt="Simbolo" width={30} height={30} />
+          <article>
+            <Image src="/images/photo-profile.png" alt="Eu" width={300} height={350} />
+            <div>
+              <p>2019 foi um ano de mudanças na minha vida. Larguei meu emprego CLT em busca de trabalhar com o que me movia e que até então, era um risco, mas se eu quisesse uma mudança eu teria que enfrentar o desafio.</p>
+              <p>O momento era vai ou racha! Bem… Eu fui. O friozinho na barriga de empreender veio junto,  noites mal dormidas pensando se tinha feito a coisa certa… Enfim, o grupo do <span>“medo”</span> estava ali comigo, mas eu fico feliz em ter tomado essa decisão.</p>
+            </div>
+          </article>
+          <article>
+            <div>
+              <p>Comecei a ter mais tempo para me dedicar com o que amo e que tenho confiança em fazer (sem contar o conforto de trabalhar em casa com minhas calças moletom). Me aperfeiçoando e buscando ajudar marcas a transmitirem sua mensagem ao mundo, a sua essência, seu universo visual de forma criativa e afetiva.</p>
+              <p>Criatividade é um exercício que amo praticar todos os dias, assim como uma <span>(duas, três e contando)</span> boa xícara de café.</p>
+              <p>O prazer seria todo meu poder compartilhá-los com você. Ah, e caso o café não seja o seu favorito, temos café também. Digo, chá!</p>
+            </div>
+            <Image src="/images/photo-profile.png" alt="Eu" width={400} height={450} />
+          </article>
+        </AboutMeContent>
+      </AboutMe>
       <Instagram />
     </>
   )
--- src/styles/pages/about.ts
+++ src/styles/pages/about.ts
@@ -25,3 +25,65 @@ export const WellcomeContent = styled.section`
     max-width: 40ch;
   }
 `
+
+export const AboutMe = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1100px;
+  margin: 2.5rem auto;
+  padding: 0 1rem;
+`
+
+export const AboutMeContent = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  article {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2.5rem;
+    margin-top: 2.5rem;
+
+    div {
+      flex: 1;
+
+      p {
+        font-size: 1rem;
+        text-align: justify;
+        font-weight: 400;
+        color: ${colors.black};
+        letter-spacing: 1.8px;
+
+        + p {
+          margin-top: 1rem;
+        }
+
+        span {
+          font-style: italic;
+        }
+      }
+    }
+
+    img {
+      border: 1px solid ${colors.black} !important;
+    }
+  }
+
+  @media (max-width: 1080px) {
+    article {
+      gap: 1rem;
+    }
+  }
+
+  @media (max-width: 800px) {
+    article {
+      flex-direction: column;
+      gap: 2.5rem;
+
+      + article {
+        flex-direction: column-reverse;
+      }
+    }
+  }
+`
