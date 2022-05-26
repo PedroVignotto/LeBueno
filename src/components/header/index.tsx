de63986cc701d667094b1b48c08eff9af6e7874c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-26 15:38:44 -0300

    fix: set visible for false when link is clicked

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,16 +7,14 @@ import { useState } from 'react'
 export function Header() {
   const [visible, setVisible] = useState(false)
 
-  const handleMenu = ():void => setVisible(!visible)
-
   return (
     <Container id="header">
       <Content visible={visible}>
         <Link href="/"><a><Image src="/images/logo-letter-black.svg" alt="Letícia Bueno" width={46} height={35} /></a></Link>
-        <button onClick={handleMenu}><span id="bars"></span></button>
+        <button onClick={() => setVisible(!visible)}><span id="bars"></span></button>
         <nav>
-          <Link href="/"><a onClick={handleMenu}>início</a></Link>
-          <Link href="/about"><a onClick={handleMenu}>sobre</a></Link>
+          <Link href="/"><a onClick={() => setVisible(false)}>início</a></Link>
+          <Link href="/about"><a onClick={() => setVisible(false)}>sobre</a></Link>
           <a href="#">serviços</a>
           <a href="#">portfólio</a>
           <a href="#">orçamento</a>
