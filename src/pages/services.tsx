b79f3e13913aaa0fb8634b9f3ef1c3f6aac1dc28
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-28 16:52:48 -0300

    feat: add main section on service page

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,4 +1,6 @@
-import { Title } from '../styles/pages/services'
+import { Button } from '../components'
+
+import { Title, Main } from '../styles/pages/services'
 
 import Head from 'next/head'
 import Image from 'next/image'
@@ -13,6 +15,46 @@ export default function Services() {
         <Image src="/images/logo/symbol-black.svg" alt="Simbolo" width={30} height={30} />
         <h1>conheça os serviços</h1>
       </Title>
+      <Main>
+        <article className="visual-identity">
+          <div>
+            <Button>identidade visual</Button>
+            <p>Muito mais do que um logotipo. Uma identidade visual é um conjunto de variações do seu logo, tipografias, paletas de cores, elementos de design e padrões estrategicamente escolhidos que capturam quem você é como empresa e deixa seu público com uma impressão duradoura.</p>
+            <p>Uma idetidade visual bem definida, criada com intenção e afeto, gera diferenciação dos demais, reconhecimento para sua empresa e consequentemente ajuda a sua marca a alcançar os clientes ideais.</p>
+            <p>Juntas, vamos traduzir e transmitir a essência e personalidade da sua marca com elementos visuais únicos e criativos.</p>
+            <p>O resultado? Uma marca que você se sente confiante para compartilhar online e offline nos próximos anos.</p>
+          </div>
+          <Image src="/images/services/visual-identity.jpg" alt="Identidade visual" width={500} height={550} />
+        </article>
+        <article className="digital-arts">
+          <Image src="/images/services/digital-arts.jpg" alt="Artes Digitais" width={500} height={550} />
+          <div>
+            <Button>artes digitais</Button>
+            <p>Em conjunto com a identidade visual, uma opção de incorporar a sua marca é usar materiais de papelaria e mídias sociais de apoio, como por exemplo:</p>
+            <ul>
+              <li>• Cartão de visitas</li>
+              <li>• Cartão interativo digital</li>
+              <li>• Capas para destaques</li>
+              <li>• Papel timbrado</li>
+              <li>• Entre outros</li>
+            </ul>
+            <p>Esses materiais ajudam a dar uma melhor visualização e a manter o seu design padronizado com sua identidade.</p>
+            <p><span>*Serviço de entrega somente digital. Não trabalho com impressos.</span></p>
+          </div>
+        </article>
+        <article className="social-network">
+          <div>
+            <Button>criativos redes sociais</Button>
+            <p>Elaboração de design da arte para postagens nas redes sociais (feed ou stories) da sua marca.</p>
+            <ul>
+              <li>• Modelos editáveis criados no Canva: O objetivo dos modelos é ajudar a você permanecer consistente, manter um perfil harmônico e coeso. As artes são sempre construídas usando os elementos da sua marca: paleta de cores, tipografia, pattern, etc.</li>
+              <li>• Criativos para as redes sociais: Aqui, eu crio a arte conforme seu conteúdo enviado e entrego a arte pronta para ser publicada.</li>
+            </ul>
+            <p><span>*O envio dos textos/conteúdos é de responsabilidade do cliente. Eu elaboro somente a arte.</span></p>
+          </div>
+          <Image src="/images/services/social-network.jpg" alt="Criativos redes sociais" width={500} height={550} />
+        </article>
+      </Main>
     </>
   )
 }
--- src/styles/pages/services.ts
+++ src/styles/pages/services.ts
@@ -1,12 +1,14 @@
 import { colors } from '../global'
 
+import { darken } from 'polished'
 import styled from 'styled-components'
 
-export const Title = styled.div`
+export const Title = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 2.5rem 1rem;
+  border-bottom: 1px solid ${colors.black};
 
   h1 {
     font-size: 0.875rem;
@@ -16,5 +18,112 @@ export const Title = styled.div`
     letter-spacing: 1.8px;
     margin-top: 1rem;
     text-align: center;
+  }
+`
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  max-width: 1100px;
+  margin: 2.5rem auto;
+  padding: 0 1rem;
+
+  article {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2.5rem;
+
+    + article {
+      margin-top: 2.5rem;
+    }
+
+    div {
+      flex: 1;
+
+      button {
+        text-transform: lowercase;
+        margin-bottom: 2.5rem;
+        font-size: 1rem;
+        font-style: italic;
+        cursor: auto;
+      }
+
+      p, ul, li {
+        font-size: 1rem;
+        text-align: justify;
+        font-weight: 400;
+        color: ${colors.black};
+        letter-spacing: 1px;
+
+        + p {
+          margin-top: 1rem;
+        }
+
+        span {
+          font-style: italic;
+        }
+      }
+
+      ul {
+        margin-top: 1rem;
+      }
+    }
+
+    img {
+      border: 1px solid ${colors.black} !important;
+    }
+  }
+
+  .visual-identity {
+    div {
+      button {
+        background: ${colors.pink};
+
+        &:hover {
+          background: ${darken(0.07, colors.pink)};
+        }
+      }
+    }
+  }
+
+  .digital-arts {
+    div {
+      button {
+        color: ${colors.white};
+        background: ${colors.gold};
+
+        &:hover {
+          background: ${darken(0.07, colors.gold)};
+        }
+      }
+    }
+  }
+
+  @media (max-width: 1080px) {
+    article {
+      gap: 1rem;
+    }
+  }
+
+  @media (max-width: 1000px) {
+    article {
+      flex-direction: column;
+      gap: 2.5rem;
+
+      div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        button {
+          align-self: center;
+        }
+      }
+    }
+
+    .digital-arts {
+      flex-direction: column-reverse;
+    }
   }
 `
