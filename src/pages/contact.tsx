4bd98ffe63fb9c2fb5759cf69576b80d6ef565e2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-01 18:31:13 -0300

    feat: add title section on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,8 @@
 import { Button, Input, Textarea } from '../components'
 
-import { Form } from '../styles/pages/contact'
+import { Title, Form } from '../styles/pages/contact'
 
+import Image from 'next/image'
 import axios from 'axios'
 import React, { useState } from 'react'
 
@@ -47,96 +48,102 @@ export default function Contact() {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <section>
+    <>
+      <Title>
+        <Image src="/images/logo/symbol-black.svg" alt="Simbolo" width={30} height={30} />
+        <h1>conte-me mais sobre <span>você</span> e sua <span>empresa</span></h1>
+      </Title>
+      <Form onSubmit={handleSubmit}>
+        <section>
+          <Input
+            label="nome *"
+            state={name}
+            setState={setName}
+          />
+          <Input
+            label="sobrenome *"
+            state={lastName}
+            setState={setLastName}
+          />
+        </section>
+        <section>
+          <Input
+            label="email *"
+            state={email}
+            setState={setEmail}
+          />
+          <Input
+            label="whatsapp *"
+            placeholder="Ex: (11) 93705-4095"
+            state={whatsApp}
+            setState={setWhatsApp}
+          />
+          <Input
+            label="de onde você é? *"
+            placeholder="Ex: São Paulo/SP"
+            state={city}
+            setState={setCity}
+          />
+        </section>
         <Input
-          label="nome *"
-          state={name}
-          setState={setName}
+          label="você tem disponibilidade para uma reunião? *"
+          description="Faz parte do processo de criação. A reunião é rápida, em torno de 20 minutinhos =)"
+          state={meeting}
+          setState={setMeeting}
         />
         <Input
-          label="sobrenome *"
-          state={lastName}
-          setState={setLastName}
-        />
-      </section>
-      <section>
-        <Input
-          label="email *"
-          state={email}
-          setState={setEmail}
+          label="qual o nome da sua marca? *"
+          description="Não trabalho com a parte de criação de naming da marca. Por isso é essencial que já possua um nome."
+          state={brand}
+          setState={setBrand}
         />
         <Input
-          label="whatsapp *"
-          placeholder="Ex: (11) 93705-4095"
-          state={whatsApp}
-          setState={setWhatsApp}
+          label="qual é o segmento em que atua ou irá atuar? *"
+          placeholder="Ex: Veterinária, Joalheria, Dentista, Arquitetura"
+          state={segment}
+          setState={setSegment}
+        />
+        <Textarea
+          label="me conte um pouco da sua empresa. quais os serviços oferecidos? o que ela faz? *"
+          state={brandDetails}
+          setState={setBrandDetails}
         />
         <Input
-          label="de onde você é? *"
-          placeholder="Ex: São Paulo/SP"
-          state={city}
-          setState={setCity}
+          label="sua marca possui site ou redes sociais? *"
+          description="Se sim, por gentileza inserir o link abaixo."
+          state={site}
+          setState={setSite}
         />
-      </section>
-      <Input
-        label="você tem disponibilidade para uma reunião? *"
-        description="Faz parte do processo de criação. A reunião é rápida, em torno de 20 minutinhos =)"
-        state={meeting}
-        setState={setMeeting}
-      />
-      <Input
-        label="qual o nome da sua marca? *"
-        description="Não trabalho com a parte de criação de naming da marca. Por isso é essencial que já possua um nome."
-        state={brand}
-        setState={setBrand}
-      />
-      <Input
-        label="qual é o segmento em que atua ou irá atuar? *"
-        placeholder="Ex: Veterinária, Joalheria, Dentista, Arquitetura"
-        state={segment}
-        setState={setSegment}
-      />
-      <Textarea
-        label="me conte um pouco da sua empresa. quais os serviços oferecidos? o que ela faz? *"
-        state={brandDetails}
-        setState={setBrandDetails}
-      />
-      <Input
-        label="sua marca possui site ou redes sociais? *"
-        description="Se sim, por gentileza inserir o link abaixo."
-        state={site}
-        setState={setSite}
-      />
-      <Input
-        label="quantos funcionários tem na sua empresa? *"
-        state={employees}
-        setState={setEmployees}
-      />
-      <Input
-        label="onde a identidade visual será usada? *"
-        placeholder="Ex: Site, Redes sociais, Papelaria/Impressos"
-        state={utilization}
-        setState={setUtilization}
-      />
-      <Textarea
-        label="além da identidade visual, de quais outros materiais você precisa? *"
-        placeholder="Ex: Cartão de visitas, Assinatura de e-mail, Envelope"
-        state={materials}
-        setState={setMaterials}
-      />
-      <Input
-        label="por onde você me encontrou? *"
-        state={find}
-        setState={setFind}
-      />
-      <Textarea
-        label="tem alguma dúvida que possa te ajudar? *"
-        description="Caso prefira, fique a vontade para entrar em contato comigo via WhatsApp: (11) 93705-4095 =)"
-        state={doubt}
-        setState={setDoubt}
-      />
-      <Button type="submit">enviar</Button>
-    </Form>
+        <Input
+          label="quantos funcionários tem na sua empresa? *"
+          state={employees}
+          setState={setEmployees}
+        />
+        <Input
+          label="onde a identidade visual será usada? *"
+          placeholder="Ex: Site, Redes sociais, Papelaria/Impressos"
+          state={utilization}
+          setState={setUtilization}
+        />
+        <Textarea
+          label="além da identidade visual, de quais outros materiais você precisa? *"
+          placeholder="Ex: Cartão de visitas, Assinatura de e-mail, Envelope"
+          state={materials}
+          setState={setMaterials}
+        />
+        <Input
+          label="por onde você me encontrou? *"
+          state={find}
+          setState={setFind}
+        />
+        <Textarea
+          label="tem alguma dúvida que possa te ajudar? *"
+          description="Caso prefira, fique a vontade para entrar em contato comigo via WhatsApp: (11) 93705-4095 =)"
+          state={doubt}
+          setState={setDoubt}
+        />
+        <Button type="submit">enviar</Button>
+      </Form>
+    </>
   )
 }
--- src/styles/pages/contact.ts
+++ src/styles/pages/contact.ts
@@ -1,4 +1,23 @@
+import { colors } from '../global'
+
 import styled from 'styled-components'
+
+export const Title = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2.5rem 1rem;
+
+  h1 {
+    font-size: 0.875rem;
+    text-transform: uppercase;
+    font-weight: 600;
+    color: ${colors.black};
+    letter-spacing: 1.8px;
+    margin-top: 1rem;
+    text-align: center;
+  }
+`
 
 export const Form = styled.form`
   display: flex;
