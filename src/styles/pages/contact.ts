47ccddad2ca13d32f7bcfcae9155fb38c989910c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-01 15:23:01 -0300

    refactor: remove unused code

--- src/styles/pages/contact.ts
+++ src/styles/pages/contact.ts
@@ -1,11 +1,8 @@
-import { colors } from '../global'
-
 import styled from 'styled-components'
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
-  align-items: c;
   justify-content: center;
   gap: 2.25rem;
   max-width: 1100px;
