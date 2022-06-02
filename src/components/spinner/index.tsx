983dcf1b070eea7e2c9deffda786fc23cd9192d4
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-02 13:07:08 -0300

    feat: add spinner component

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+import { FiRotateCw } from 'react-icons/fi'
+
+export function Spinner() {
+  return (
+    <Container>
+      <FiRotateCw />
+    </Container>
+  )
+}
