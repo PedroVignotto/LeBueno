2e8fd27252d54f6ef2de814a49b08316ac91396d
PedroVignotto <pedro.vignotto@hotmail.com>
2022-06-07 19:01:46 -0300

    refactor: change instagram projects

--- src/components/instagram/index.tsx
+++ src/components/instagram/index.tsx
@@ -8,14 +8,14 @@ export function Instagram() {
       <Content>
         <h3>acompanhe meu dia a dia no instagram: </h3>
         <div>
-          <a href="https://www.instagram.com/p/CdySLzsrfLd/" target="_blank" rel="noreferrer">
-            <Image src="/images/instagram/project-1.png" alt="ADRI MÜLLER PHOTOGRAPHY" width={250} height={250} />
+          <a href="https://www.instagram.com/p/CYUxU_RM_6k/" target="_blank" rel="noreferrer">
+            <Image src="/images/instagram/project-1.jpg" alt="FERNANDA CERIONI" width={250} height={250} />
           </a>
           <a href="https://www.instagram.com/p/Cc6VcGXsvga/" target="_blank" rel="noreferrer">
             <Image src="/images/instagram/project-2.png" alt="DEVIR ARQUITETURA" width={250} height={250} />
           </a>
-          <a href="https://www.instagram.com/p/CcgaA8ZMZZZ/" target="_blank" rel="noreferrer">
-            <Image src="/images/instagram/project-3.png" alt="CERES VETERINÁRIA" width={250} height={250} />
+          <a href="https://www.instagram.com/p/CVL6wFhLvrz/" target="_blank" rel="noreferrer">
+            <Image src="/images/instagram/project-3.jpg" alt="COMUNITÁ" width={250} height={250} />
           </a>
           <a href="https://www.instagram.com/p/CZhROrSMzIh/" target="_blank" rel="noreferrer">
             <Image src="/images/instagram/project-4.png" alt="COM AL[MACRAMÊ]" width={250} height={250} />
